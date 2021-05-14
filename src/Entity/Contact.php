c32c1158551406c9ad98627ac1c4bf63c44dba2f
jessdeni <80536274+jessdeni@users.noreply.github.com>
2021-05-14 11:52:03 +0200

    formcontact

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=ContactRepository::class)
  */
-class Contact
+class Contact implements ContactRepository
 {
     /**
      * @ORM\Id
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class ContactType extends AbstractType
         $builder
             ->add('nom')
             ->add('prenom')
-            ->add('email')
+            ->add('email',EmailType::class)
             ->add('message')
         ;
     }
