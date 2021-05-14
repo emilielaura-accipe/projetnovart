d196d786ca04c739a9c3ab353986f1b556fe5ab2
jessdeni <80536274+jessdeni@users.noreply.github.com>
2021-05-14 14:27:41 +0200

    formcontact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
-use  Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
