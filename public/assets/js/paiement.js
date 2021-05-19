6157149c05cffb48be847877b843e629fbcbc945
IvanaMilo <81551984+IvanaMilo@users.noreply.github.com>
2021-05-19 09:55:26 +0200

    Create paiement.js

---
+++ public/assets/js/paiement.js
@@ -0,0 +1,27 @@
+      var stripe = Stripe('pk_test_51Is9hUI5JMdcaI0aMfj5PygDsO41DUOqQhJBFcJ6oPbYmOU0Z0w8SBHi8lAxYbWRyvWfWOIB8alrZYfr5ZH3FJvX00yvc7tt77');
+      var checkoutButton = document.getElementById('checkout-button');
+
+      checkoutButton.addEventListener('click', function() {
+        // Create a new Checkout Session using the server-side endpoint you
+        // created in step 3.
+        fetch('/create-checkout-session', {
+          method: 'POST',
+        })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(session) {
+          return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function(result) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, you should display the localized error message to your
+          // customer using `error.message`.
+          if (result.error) {
+            alert(result.error.message);
+          }
+        })
+        .catch(function(error) {
+          console.error('Error:', error);
+        })
+      });
